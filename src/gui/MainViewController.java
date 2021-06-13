b23dae5913505222279e3b66e8f9886c8b1db2cd
Kaian Lemos <kaianlemos18@outlook.com>
2021-06-13 10:56:07 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -63,7 +63,7 @@ public class MainViewController implements Initializable {
 	}
 
 	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
+	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
 
 	}
